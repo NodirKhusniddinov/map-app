ded94d32b134972d3b0894663c81d3d708a27142
NodirKhusniddinov <nodirkhusniddinov@MacBook-Air-Nodir.local>
2024-09-09 14:21:12 +0500

    starter

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="container mx-auto px-8 py-8">
+      <h1 className=" text-center font-bold text-xl">Map app</h1>
+    </main>
+  );
+}
+
+export default App;
